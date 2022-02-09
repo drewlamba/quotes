ba4faa48d1dc200ea9a8e6718b27b8ebb6e93e8b
drewlamba <85325222+drewlamba@users.noreply.github.com>
2022-02-09 13:41:56 +0200

    Add files via upload

---
+++ quotes/controller/quoteController.js
@@ -0,0 +1,81 @@
+const Quote = require('../models/quotes')
+const quote_index = (req, res) => {
+    Quote.find().sort({ createAt: -1 })
+        .then(result => {
+            res.render('index', {quotes: result, title: 'All Quotes'})
+        }) 
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const quote_details = (req, res) => {
+    const id = req.params.id
+    Quote.findById(id)
+        .then(result => {
+            res.render('details', {quote: result, title: 'Quote Details'})
+        })
+        .catch(err => {
+            console.log(err)
+            res.render('404', {title: 'Page not found'})
+        })
+}
+
+const quote_create_get = (req, res) => {
+    res.render('create', {title: 'Write a new Quote'})
+}
+
+
+
+const quote_create_post = (req, res) => {
+
+    var outCount = 0;
+    Quote.count()
+        .then(count => {
+            outCount = count;
+            console.log(outCount)
+            const quote = new Quote({
+                author: req.body.author,
+                details: req.body.details,
+                quote_type: req.body.quote_type,
+                source: req.body.source,
+                value: outCount
+            })
+
+            quote.save()
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/quotes')
+                })
+                .catch(err => {
+                    console.log('err')
+                })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    console.log(outCount)
+
+
+}
+
+const quote_delete = (req, res) => {
+    const id = req.params.id
+    Quote.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/quotes'})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+
+
+module.exports = {
+    quote_index,
+    quote_details,
+    quote_create_get,
+    quote_create_post,
+    quote_delete
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const http = require('http')
+const quotesRoutes = require('./routes/quotesRoutes');
+const apiRoutes = require('./routes/apiRoutes')
+const userRoutes = require('./routes/usersRoutes')
+const Quote = require('./models/quotes')
+const fs = require('fs');
+const app = express()
+
+const dbURI = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@nodewecarecluster.jl8pq.mongodb.net/initialDb?retryWrites=true&w=majority`
+
+mongoose.connect(dbURI, { userNewParser: true, useUnifiedTopology: true})
+        .then(result => app.listen(process.env.PORT || 3009))
+        .catch(err => console.log(err))
+
+
+app.set('view engine', 'ejs')
+
+// middleware
+
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('combined'))
+
+
+
+app.use((req, res, next) => {
+    res.locals.path = req.path
+    next()
+})
+
+
+// routes
+app.get('/', (req, res) => {
+    res.redirect('/quotes')
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' })
+  });
+
+app.use('/api', apiRoutes)
+
+  // quotes routes
+app.use('/quotes', quotesRoutes)
+
+app.use('/users', userRoutes)
+
+  // 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' })
+})
